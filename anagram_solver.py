ea3abad3ee5c0f01279a16508fa1fa446c044ca2
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-03-06 16:06:30 +1100

    comments and clarity

--- anagram_solver.py
+++ anagram_solver.py
@@ -1,28 +1,27 @@
 import json
 
+# Alphabetically sorts the characters in a string
 def sort_word(word):
     return ''.join(sorted(word.upper()))
 
+# So far only finds if a base or base minus one char has anagrams
 def find_best_ana(base):
     base_unique = ''.join(set(base))
     ana_list = []
+    if base in word_dict.keys():
+        ana_list.append(base)
     for i in base_unique:
         base_cat = base.replace(i, '', 1)
         if base_cat in word_dict.keys():
             ana_list.append(base_cat)
-    
-
+    return ana_list
 
 with open("Dictionary/hash_dict.json", "r") as file:
     word_dict = json.load(file)
 
+# for each line, check for anagrams
 with open("test.txt", "r") as file:
     lines = file.readlines()
     for line in lines:
         base = sort_word(line.strip())
-        if base in word_dict.keys():
-            print(word_dict[base])
-        else:
-            print("sad")
-            print(f"1st letter?: {line[0]}")
-            print(f"3rd letter?: {line[2]}")
+        print(find_best_ana(base))
