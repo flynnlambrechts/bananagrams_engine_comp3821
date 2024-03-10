caa0f6255739c055c58f86a0f6ab2d5c0bfcdf2e
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-03-10 15:14:38 +1100

    Add to-string implementation

--- data_structure/board.py
+++ data_structure/board.py
@@ -1,21 +1,43 @@
 class Board:
     def __init__(self) -> None:
         self.tiles: dict[tuple[int, int]] = {}
-        # self.max_row = None
-        # self.max_col = None
-        # self.min_row = None
-        # self.min_col = None
+        
+    # This allows us to use print(board)
+    def __str__(self) -> str:
+        s = ''
+        min_row = min([row for row, _ in self.tiles])
+        min_col = min([col for _, col in self.tiles])
+        
+        cur_row = min_row
+        cur_col = min_col
+        for (row, col), value in sorted(self.tiles.items(), key=lambda item: (item[0][0], item[0][1])):
+            while cur_row < row:
+                cur_row += 1
+                cur_col = 0
+                s += '\n'
+            while cur_col < col:
+                cur_col += 1
+                s += ' '
+            
+            s += value
+            cur_col += 1
+            
+        return s
+    
         
     def add_tile(self, tile: str, row: int, col: int) -> None:
+        if len(tile) != 1:
+            raise ValueError('Tile must be one character long')
         if (row, col) in self.tiles:
             raise ValueError(f'There is already a tile at ({row}, {col})')
         
-        adjacent = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
-        
-        if all([coord not in self.tiles for coord in adjacent]):
-            # No character tiles around it
-            self.tiles[(row, col)] = tile
-            return
+        # # Future nodes for caching sequences/words
+        # adjacent = [(row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)]
+        # 
+        # if all([coord not in self.tiles for coord in adjacent]):
+        #     # No character tiles around it
+        #     self.tiles[(row, col)] = tile
+        #     return
         
         self.tiles[(row, col)] = tile
         
