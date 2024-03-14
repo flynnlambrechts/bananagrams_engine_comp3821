0c1ebbec60a4398c22ce68b5858d3b526aab66e2
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-03-14 21:55:40 +1100

    updating test file path

--- test_run_algorithm.py
+++ test_run_algorithm.py
@@ -1,9 +1,9 @@
 from Classes.trie import *
 from run_algorithm import *
 
-with open("starting_letters_x100.txt", "r") as file:
+with open("starting_letters_x1000.txt", "r") as file:
     trie = Trie()
-    trie.make_trie("Dictionary/word_dictionary.txt")
+    trie.make_trie("Classes/word_dictionary.txt")
 
     for line in file.readlines():
         base = ''.join(sorted(line.strip()))
