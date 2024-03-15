6ee255d0b3b230b66df1fc9fbed1baed247ea71b
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-03-15 15:56:14 +1100

    creating in-board tracking of where words can be built. work in progress

--- Game_Structure/board.py
+++ Game_Structure/board.py
@@ -1,29 +1,78 @@
+MAX_LIMIT = 50
+
+class Tile:
+    def __init__(self, board, row, col, char):
+        self.coords = tuple[row, col]
+        self.char = char
+        self.lims = []
+        self.board = board
+
+        self.lims.append(self.find_lims(1,0))
+        self.lims.append(self.find_lims(-1,0))
+        self.lims.append(self.find_lims(0,1))
+        self.lims.append(self.find_lims(0,-1))
+
+    def find_lims(self, row, col):
+        count = 0
+        cur_row = self.tile.coords[0]
+        cur_col = self.tile.coords[1]
+        no_barriers = True
+        while(no_barriers):
+            cur_row += row
+            cur_col += col
+            check_tiles = [(cur_row + row, cur_col + col),
+                           (cur_row + (row + 1) % 2, cur_row + (col + 1) % 2),
+                           (cur_row - ((row + 1) % 2), cur_row - ((col + 1) % 2))]
+            for tile in check_tiles:
+                if tile in self.board.tiles:
+                    no_barriers = False
+                    lims_pos = (abs(row) * (row - 1) / -2) + (abs(col) * ((row - 1) / -2 + 2))
+                    updateable = self.board.tiles[tile].lims[lims_pos]
+                    updateable = min(updateable, count)
+            if not ((self.board.min_row() <= cur_row <= self.board.max_row()) and (self.board.min_col() <= cur_col <= self.board.max_col())):
+                return MAX_LIMIT
+            count += 1
+        return count        
+
+        
 class Board:
     def __init__(self) -> None:
         self.tiles: dict[tuple[int, int]] = {}
-        
+
+    def min_row(self):
+        return min([row for row, _ in self.tiles])
+    
+    def max_row(self):
+        return max([row for row, _ in self.tiles])
+    
+    def min_col(self): 
+        return min([col for _, col in self.tiles])
+
+    def max_col(self): 
+        return max([col for _, col in self.tiles])
+
     # This allows us to use print(board)
     def __str__(self) -> str:
         if not self.tiles:
             return "Board is empty"
         col_delim = " "
 
-        min_row = min([row for row, _ in self.tiles])
-        max_row = max([row for row, _ in self.tiles])
-        min_col = min([col for _, col in self.tiles])
-        max_col = max([col for _, col in self.tiles])
+        # min_row = min([row for row, _ in self.tiles])
+        # max_row = max([row for row, _ in self.tiles])
+        # min_col = min([col for _, col in self.tiles])
+        # max_col = max([col for _, col in self.tiles])
 
 
         header = 4*" " + col_delim.join(
             map(lambda x: x[-1],
-                map(str, range(min_col, max_col + 1))
+                map(str, range(self.min_col, self.max_col + 1))
             )
             ) + "\n"
 
-        s =  header + f"{min_row:>4}"
+        s =  header + f"{self.min_row:>4}"
         
-        cur_row = min_row
-        cur_col = min_col
+        cur_row = self.min_row
+        cur_col = self.min_col
         for (row, col), value in sorted(self.tiles.items(), key=lambda item: (item[0][0], item[0][1])):
             while cur_row < row:
                 cur_row += 1
---
+++ test_limits.py
@@ -0,0 +1,19 @@
+from Game_Structure.game import Game
+from Classes.trie import Trie
+from run_algorithm import long_start_word
+
+s_trie = Trie("sort")
+s_trie.make_trie("Classes/word_dictionary.txt")
+
+game = Game()
+game.setup()
+print(game)
+print(game.hand)
+words = s_trie.all_subwords(game.hand)
+word = long_start_word(words).word
+print(f"word: {word}")
+game.play_word(word, 0, 0, 0, False)
+print(game)
+# words = s_trie.find_two_letters("E")
+# for word in words:
+#     print(word.word)
