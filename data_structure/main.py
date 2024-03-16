8a50853c0d405467553b2950f41d34a9978515c9
Flynn Lambrechts <flynn.lambrechts@gmail.com>
2024-03-16 15:20:43 +1100

    Fix: Column missalignment on print

--- data_structure/main.py
+++ data_structure/main.py
@@ -3,12 +3,28 @@ VERTICAL = 1
 HORIZONTAL = 0
 
 
+
+print("")
+
+board2 = Board()
+board2.add_word("EEE", 0, 4, 1)
+print(board2)
+
+print("")
+
+board3 = Board()
+board3.add_word("EEEEE", 0, 0, 0)
+board3.add_word("EEE", 0, 0, 1)
+board3.add_word("EEE", 0, 4, 1)
+print(board3)
+
+print("")
+
 board = Board()
-board.add_word("HELLO", 1, 1, HORIZONTAL)
+board.add_word("hello", 1, 1, HORIZONTAL)
 board.add_word("world", 2, 5, HORIZONTAL)
 board.add_word("lower", 2, 8, VERTICAL)
 board.add_word("partner", 6, 8, HORIZONTAL, reverse=True)
 board.add_word("watermelon",4, 8, HORIZONTAL)
 
 print(board)
-
