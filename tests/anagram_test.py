01fa742da5566d6b20ac0c470c0364c6d608b076
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-03-27 22:04:34 +1100

    updated anagram_test to work again

--- tests/anagram_test.py
+++ tests/anagram_test.py
@@ -1,63 +1,64 @@
 import time
-from src.Game.BananaPouch.BananaPouch import BananaPouch
-from src.Algorithm.Trie.Trie import Trie
+from src.pouch import Pouch
+from src.trie import Trie
+from pathlib import Path
 
-
-s_trie = Trie()
-f_trie = Trie("forward")
-r_trie = Trie("reverse")
-s_trie.parse_file("word_dictionary.txt")
-f_trie.parse_file("word_dictionary.txt")
-r_trie.parse_file("word_dictionary.txt")
-pouch = BananaPouch()
+this_directory = Path(__file__).parent.resolve()
+dictionary = this_directory / '..' / 'assets' / 'word_dictionary.txt'
+print(f'[Initializing]')
+all_words = Trie(mode='sort', dictionary_path=dictionary)
+forward_words = Trie('forward', dictionary_path=dictionary)
+reverse_words = Trie('reverse', dictionary_path=dictionary)
+pouch = Pouch()
 
 print("tries made!")
 base = "IP"
 print(f"base: {base}")
 anchor = "CRA"
 print(f"anchor: {anchor}")
-f_list = f_trie.all_subwords(base, anchor)
+f_list = forward_words.all_subwords(base, anchor)
 print("forward:")
 for word in f_list:
     print(word.string, end=", ")
-r_list = r_trie.all_subwords(base, anchor)
+r_list = reverse_words.all_subwords(base, anchor)
 print("\n\nreverse:")
 for word in r_list:
     print(word.string, end=", ")
-s_list = s_trie.all_subwords(base, anchor)
+s_list = all_words.all_subwords(base, anchor)
 print("sorted:")
 for word in s_list:
     print(word.string, end=", ")
 
+durations = []
+nodes_visited = []
+words_found = []
+big_words_found = []
+for i in range(1000):
+    pouch = Pouch()
+    base = ''.join(pouch.get_starting_tiles())
+    print(f"run {i}: {base}")
 
-with open("starting_letters_x100.txt", "r") as file:
-    durations = []
-    words_found = []
-    nodes_visited = []
-    lines = file.readlines()
-    j = 0
+    start = time.time()
+    word_count = all_words.all_subwords(base)
+    end = time.time()
 
-    for line in lines:
-        base = ''.join(sorted(line.strip().upper()))
-        j += 1
-        print(f"run {j}: {base}")
+    durations.append(end-start)
+    # Note that the nodes visited part of all_subwords is by default commented out
+    # Since it's not useful for actually doing stuff
+    # So it needs to be uncommented for that to work.
+    nodes_visited.append(word_count.pop())
+    words_found.append(len(word_count))
+    big_word_count = sum(len(item.string) >= 9 for item in word_count)
+    big_words_found.append(big_word_count)
+    print(
+        f"found {words_found[-1]} words in {end - start} seconds!")
 
-        start = time.time()
-        word_count = s_trie.all_subwords(base)
-        end = time.time()
+print(f"av. time: {sum(durations)/len(durations)}")
+print(f"Total time: {sum(durations)}")
+print(f"av. words found: {sum(words_found)/len(words_found)}")
+print(f"max words found: {max(words_found)}")
+print(f"av. long words found: {sum(big_words_found)/len(big_words_found)}")
+print(f"min. long words found: {min(big_words_found)}")
+print(f"max. long words found: {max(big_words_found)}")
 
-        durations.append(end-start)
-        # Note that the nodes visited part of all_subwords is by default commented out
-        # Since it's not useful for actually doing stuff
-        # So it needs to be uncommented for that to work.
-        nodes_visited.append(word_count.pop())
-        words_found.append(len(word_count))
-
-        print(
-            f"found {words_found[-1]} words in {end - start} seconds by looking at \
-                {nodes_visited[-1].word_string} nodes!")
-
-    print(f"av. time: {sum(durations)/len(durations)}")
-    print(f"Total time: {sum(durations)}")
-    print(f"av. words found: {sum(words_found)/len(words_found)}")
-    print(f"av. nodes visited: {sum(nodes_visited)/len(nodes_visited)}")
+# print(f"av. nodes visited: {sum(nodes_visited)/len(nodes_visited)}")
