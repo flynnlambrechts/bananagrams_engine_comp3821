9e77396f88228fa7493f6a9f7f061ee73a48f561
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-03-29 11:32:18 +1100

    algorithm for scoring words with stranding

--- src/algorithms.py
+++ src/algorithms.py
@@ -2,6 +2,12 @@ from word import Word
 from tile import Tile
 from constants import NO_SPACE_FOR_WORD, HORIZONTAL, VERTICAL
 
+is_prefix_of = {'A': 16194, 'B': 15218, 'C': 25015, 'D': 16619, 'E': 11330, 'F': 10633, 'G': 9351, 'H': 10524, 'I': 9604, 'J': 2311, 'K': 3361, 'L': 8058, 'M': 15811, 'N': 6564, 'O': 8895, 'P': 24327, 'Q': 1411, 'R': 15014, 'S': 31986, 'T': 14563, 'U': 9522, 'V': 4590, 'W': 5921, 'X': 309, 'Y': 1036, 'Z': 1159}
+is_suffix_of = {'A': 5560, 'B': 371, 'C': 6121, 'D': 25039, 'E': 28261, 'F': 534, 'G': 20216, 'H': 3293, 'I': 1601, 'J': 12, 'K': 2327, 'L': 8586, 'M': 4620, 'N': 12003, 'O': 1887, 'P': 1547, 'Q': 10, 'R': 14784, 'S': 107294, 'T': 13933, 'U': 379, 'V': 45, 'W': 610, 'X': 583, 'Y': 19551, 'Z': 159}
+pair_start_count = {'A': 16, 'B': 5, 'C': 1, 'D': 4, 'E': 13, 'F': 3, 'G': 3, 'H': 5, 'I': 6, 'J': 2, 'K': 4, 'L': 3, 'M': 7, 'N': 5, 'O': 17, 'P': 4, 'Q': 1, 'R': 1, 'S': 4, 'T': 4, 'U': 8, 'V': 0, 'W': 2, 'X': 2, 'Y': 4, 'Z': 3}
+pair_end_count = {'A': 15, 'B': 2, 'C': 0, 'D': 4, 'E': 15, 'F': 3, 'G': 2, 'H': 6, 'I': 14, 'J': 0, 'K': 1, 'L': 2, 'M': 6, 'N': 5, 'O': 17, 'P': 2, 'Q': 0, 'R': 4, 'S': 5, 'T': 5, 'U': 6, 'V': 0, 'W': 3, 'X': 3, 'Y': 7, 'Z': 0}
+
+
 def where_to_play_word(word_str: str, anchor: Tile) -> tuple[int, int]:
         '''
         Given an anchor and a word, determines if there's space to play it. 
@@ -41,7 +47,7 @@ def where_to_play_word(word_str: str, anchor: Tile) -> tuple[int, int]:
                     return (anchor_index, HORIZONTAL)
         return NO_SPACE_FOR_WORD
 
-def long_with_lowest_rank(subwords, anchor: Tile = None, closeness_to_longest = 0) -> Word:
+def long_with_best_rank(subwords, rank_strategy = "strand",anchor: Tile = None, closeness_to_longest = 0) -> Word:
     '''
     Finds a long subword with the lowest letter_ranking
     (Means that it uses letters that appear less in the dictionary),
@@ -65,9 +71,10 @@ def long_with_lowest_rank(subwords, anchor: Tile = None, closeness_to_longest = 
 
     if len(long_words) == 0:
         return None
-    min_word = min(long_words, key=lambda word: word.letter_ranking / len(word.string))
-    return min_word
-
+    if rank_strategy == "strand":
+        return max(long_words, key=score_word_simple_stranding)
+    else:
+        return min(long_words, key=lambda word: word.letter_ranking / len(word.string))
 
 '''None of the below is actually being used'''
 def anchor_ranking(tiles: dict[tuple[int, int]]) -> list:
@@ -87,3 +94,31 @@ def _eval_anchor_candidate(tile: Tile) -> int:
             tile.lims.up() > 8 and tile.lims.down() > 8):
         score += 100
     return score
+
+# def best_next_strand(subwords, )
+
+'''so much room for more interesting stuff, but it's a start'''
+def score_word_simple_stranding(word_str, min_length = 0):
+    if len(word_str < min_length):
+        return -1000000
+    # all_other_letters = self._all_other_letters(word_str)
+    if len(word_str) > 2:
+        word_middle = word_str[1:-1]
+        middle_score = sum(pair_end_count[char] + pair_start_count[char] for char in word_middle) / (len(word_str) - 2)
+    else:
+        middle_score = 0
+    
+    edge_score = (is_prefix_of[word_str[0]] + is_suffix_of[word_str[0]] + 
+                  is_prefix_of[word_str[-1]] + is_suffix_of[word_str[-1]] + 
+                  (pair_end_count[word_str[0]] + pair_start_count[word_str[0]] +
+                  pair_end_count[word_str[-1]] + pair_start_count[word_str[-1]]) * 1000)
+    
+    
+    return edge_score - 1000 * middle_score
+
+def _all_other_letters(self, word_str):
+    all_other_letters = self.hand
+    for char in word_str:
+        all_other_letters.replace('',char,1)
+    all_other_letters += ''.join([tile.string for tile in self.board.tiles.values()])
+    return all_other_letters
