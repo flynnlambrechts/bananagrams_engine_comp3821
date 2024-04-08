8a3c22c22e86d000ae87b31b36c01af2f5691005
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-04-09 00:33:33 +1000

    Main to use two letter player

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ from game import Game
 from players.StandardPlayer import StandardPlayer
 from players.StrandingPlayer import StrandingPlayer
 from players.PseudoPlayer import PseudoPlayer
+from players.TwoLetterJunkStrandingPlayer import TwoLetterJunkStrandingPlayer
 
 game = Game()
-game.add_player(StrandingPlayer(game))
-game.add_player(PseudoPlayer(game))
-game.add_player(PseudoPlayer(game))
+game.add_player(TwoLetterJunkStrandingPlayer(game))
+# game.add_player(PseudoPlayer(game))
+# game.add_player(PseudoPlayer(game))
 
 game.start()
