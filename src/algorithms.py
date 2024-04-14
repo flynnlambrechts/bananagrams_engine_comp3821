945c5fb3ebb300727b8267be369dfec36398beb3
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-04-14 13:45:14 +1000

    fix + comments

--- src/algorithms.py
+++ src/algorithms.py
@@ -165,6 +165,12 @@ def _all_other_letters(self, word_str):
     return all_other_letters
 
 def score_word_hand_balance(word_str: str, hand: str, anchor: str, min_length: int = 0):
+    '''Basic concept: function that (almost) always gives positive points for playing tiles, but gives more points if you play letters that are more overrepresented. 
+    
+    Define error as |proportion of letter in hand - proportion of letter in game|.
+    
+    Score is the change in the sum of error for every letter'''
+
 
     '''Notes to consider: if hand is close to perfectly balanced, nothing will look very good, and rare letters will be the last to be played. So maybe this algorithm should only run if the hand is at a particular level of unbalance. Or, use this algo to pick between the top words selected through some other means'''
     if len(word_str) < min_length:
@@ -180,8 +186,8 @@ def score_word_hand_balance(word_str: str, hand: str, anchor: str, min_length: i
     for char in set(hand):
         initial_unbalance += abs(hand.count(char)/hand_len - letter_distribution[char]/144)
     post_unbalance = 0
-    for char in set(hand_after_playing):
-        post_unbalance += abs(hand.count(char)/hand_after_playing_len - letter_distribution[char]/144)
+    for char in set(hand):
+        post_unbalance += abs(hand_after_playing.count(char)/hand_after_playing_len - letter_distribution[char]/144)
     
     return initial_unbalance - post_unbalance
      
