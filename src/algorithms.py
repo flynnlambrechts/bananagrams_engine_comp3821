d10a33b49fcdaefd954b2ee9cf20d0ed5916a0b0
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-04-17 09:46:09 +1000

    added algos from elsewhere

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,6 +1,7 @@
 from word import Word
 from board.tile import Tile
-from constants import NO_SPACE_FOR_WORD, HORIZONTAL, VERTICAL, is_prefix_of, is_suffix_of, pair_start_count, pair_end_count
+from constants import NO_SPACE_FOR_WORD, HORIZONTAL, VERTICAL, is_prefix_of, is_suffix_of, pair_start_count, pair_end_count, TOTAL_TILE_COUNT
+from pouch import letter_distribution
 
 
 def where_to_play_word(word_str: str, anchor: Tile) -> tuple[int, int]:
@@ -136,6 +137,48 @@ def _eval_anchor_candidate(tile: Tile) -> int:
     return score
 
 # def best_next_strand(words, )
+
+
+def score_word_hand(word_str, hand_str='', min_length=0):
+    '''Could incorporate the hand_str into the scoring depending on the hand aswell'''
+    result = 0
+    for char in word_str:
+        result += 10000000 - (pair_end_count[char] + pair_start_count[char])
+
+    if ('V' in word_str):
+        return 100000000
+    if ('Q' in word_str):
+        return 100000000
+    return result
+
+
+def score_word_hand_balance(word_str: str, hand: str, anchor: str, min_length: int = 0):
+    '''Basic concept: function that (almost) always gives positive points for playing tiles, but gives more points if you play letters that are more overrepresented. 
+
+    Define error as |proportion of letter in hand - proportion of letter in game|.
+
+    Score is the change in the sum of error for every letter'''
+
+    '''Notes to consider: if hand is close to perfectly balanced, nothing will look very good, and rare letters will be the last to be played. So maybe this algorithm should only run if the hand is at a particular level of unbalance. Or, use this algo to pick between the top words selected through some other means'''
+    if len(word_str) < min_length:
+        return 0
+    hand_after_playing = hand
+    played_tiles = word_str.replace(anchor, '', 1)
+    for char in played_tiles:
+        hand_after_playing.replace(char, '', 1)
+
+    hand_len = len(hand)
+    hand_after_playing_len = len(hand_after_playing)
+    initial_unbalance = 0
+    for char in set(hand):
+        initial_unbalance += abs(hand.count(char) /
+                                 hand_len - letter_distribution[char]/144)
+    post_unbalance = 0
+    for char in set(hand):
+        post_unbalance += abs(hand_after_playing.count(char) /
+                              hand_after_playing_len - letter_distribution[char]/144)
+
+    return initial_unbalance - post_unbalance
 
 
 '''so much room for more interesting stuff, but it's a start'''
