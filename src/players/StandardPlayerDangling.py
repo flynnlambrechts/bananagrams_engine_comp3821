e683cd79af50391f4ad1f1e355691f6892446bd2
Kyler Blackburn <kyler.blackburn@gmail.com>
2024-04-19 17:08:02 +1000

    solved infinite loop issue

--- src/players/StandardPlayerDangling.py
+++ src/players/StandardPlayerDangling.py
@@ -4,33 +4,36 @@ class StandardPlayerDangling(StandardPlayer):
 
     def __init__(self, game, id: int, word_scorer) -> None:
         super().__init__(game, id, word_scorer=word_scorer)
-        self.hand_at_last_restructure = ''
+        self.previous_hands = []
 
     def play_turn(self):
         self.show_board()
         return super().play_turn()
 
     def peel(self):
-        self.hand_at_last_restructure = ''
+        self.previous_hands = []
         return super().peel()
 
     def restructure_board(self):
         self.show_board()
+        old_hand = self.hand
         dangling_tiles = self.board.remove_dangling()
-        if sorted(self.hand) == self.hand_at_last_restructure:
+        for tile in dangling_tiles:
+                self.hand += tile.char
+        post_removal_hand = sorted(self.hand)
+        if any(prev_hand == post_removal_hand for prev_hand in self.previous_hands):
+            self.speak("DANGLING", "Found a previous hand, now dumping")
             worst_letter_in_hand = min(
                 self.hand, key=lambda char: letter_count[char])
             self.game.dump(self, worst_letter_in_hand)
-            if len(self.hand_at_last_restructure) == len(self.hand):
+            self.previous_hands = []
+            if len(post_removal_hand) == len(self.hand):
                 exit(1)
-        if len(dangling_tiles) == 0:
+        elif len(dangling_tiles) == 0:
             exit(1)
             return super().restructure_board()
 
         else:
-            old_hand = self.hand
-            for tile in dangling_tiles:
-                self.hand += tile.char
-            self.hand_at_last_restructure = sorted(self.hand)
+            self.previous_hands.append(sorted(self.hand))
             self.speak("DANGLING", f"Found {len(dangling_tiles)} dangling tiles, old_hand={old_hand}, new_hand={self.hand}")
             self.play_turn()
