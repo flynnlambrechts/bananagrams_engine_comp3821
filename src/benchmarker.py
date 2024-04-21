777fd0a7bb55c64782fe7f95ad17246517c3b94a
Julian <z5425206@ad.unsw.edu.au>
2024-04-21 14:39:54 +1000

    ran benchmarking two player all algos all scoreres and 8player all algos all scorers when able to, didn't run with CCCCCCCC for all because it was really slow and sometimes caused errors plus t here were so many fails that it probably isn't going to work well for 8 player

--- src/benchmarker.py
+++ src/benchmarker.py
@@ -104,7 +104,7 @@ def benchmark_game(i, j, players, times, winners, fail_counts, word_scorers):
 
 
 if __name__ == '__main__':
-    iterations = 250
+    iterations = 500
     targets = [
         # 'ppn',
         # 'ppn',
